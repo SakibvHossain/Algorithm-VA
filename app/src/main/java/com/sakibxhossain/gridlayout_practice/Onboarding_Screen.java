9a0f2969fd9921a912d47581719bacd1ba8e51c4
XSakibxHossainX <sakibxhossian021@gmail.com>
2023-02-11 22:27:14 +0600

    added anim files

---
+++ app/src/main/java/com/sakibxhossain/gridlayout_practice/Onboarding_Screen.java
@@ -0,0 +1,14 @@
+package com.sakibxhossain.gridlayout_practice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Onboarding_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onboarding_screen);
+    }
+}
---
+++ app/src/main/java/com/sakibxhossain/gridlayout_practice/Sign_Up.java
@@ -0,0 +1,14 @@
+package com.sakibxhossain.gridlayout_practice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Sign_Up extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
