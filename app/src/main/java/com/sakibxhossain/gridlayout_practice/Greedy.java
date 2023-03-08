d826829d88982ff0fee597869bfcd1ecde314eaf
XSakibxHossainX <sakibxhossian021@gmail.com>
2023-03-08 17:29:57 +0600

    little change

--- app/src/main/java/com/sakibxhossain/gridlayout_practice/Greedy.java
+++ app/src/main/java/com/sakibxhossain/gridlayout_practice/Greedy.java
@@ -2,12 +2,17 @@ package com.sakibxhossain.gridlayout_practice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.GridView;
 import android.widget.ImageView;
 
 import com.sakibxhossain.gridlayout_practice.helper.Greedy_Adapter;
+import com.sakibxhossain.gridlayout_practice.helper.Greedy_ClickListener;
 import com.sakibxhossain.gridlayout_practice.helper.Tree_Adapter;
+import com.sakibxhossain.gridlayout_practice.helper.Tree_ClickListener;
 
 public class Greedy extends AppCompatActivity {
 
@@ -24,11 +29,39 @@ public class Greedy extends AppCompatActivity {
     GridView sorting_gridview;
     Greedy_Adapter greedy_adapter;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_greedy);
 
+        text_ber_back_button = findViewById(R.id.greedy_bar_button);
+        sorting_gridview = findViewById(R.id.greedy_Section_gridView);
 
+
+        greedy_adapter = new Greedy_Adapter(this, algo_title, algo_difficulty, new Greedy_ClickListener() {
+            @Override
+            public void onItemClicked(int position) {
+                switch (position) {
+                    case 0:
+                        Intent btt = new Intent(Greedy.this, Longest_Common_Subsequence_Description.class);
+                        startActivity(btt);
+                        break;
+                    case 1:
+                    case 2:
+                    default:
+                        System.out.println("Looking forward to the Weekend");
+                }
+            }
+        });
+        sorting_gridview.setAdapter(greedy_adapter);
+
+        text_ber_back_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Greedy.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/sakibxhossain/gridlayout_practice/Longest_Common_Subsequence_Analysis.java
@@ -0,0 +1,14 @@
+package com.sakibxhossain.gridlayout_practice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Longest_Common_Subsequence_Analysis extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_longest_common_subsequence_analysis);
+    }
+}
